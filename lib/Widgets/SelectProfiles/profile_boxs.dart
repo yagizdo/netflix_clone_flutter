62e345b774df5879935692282d8e7f4da9a98774
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-04-19 17:02:54 +0300

    created profile_boxs.dart

---
+++ lib/Widgets/SelectProfiles/profile_boxs.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProfileBoxs extends StatelessWidget {
+  const ProfileBoxs({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Row(
+          children: [],
+        )
+      ],
+    );
+  }
+}
