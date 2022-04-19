226bcbb12bb37c2accb23411cf7a251fd30d6c98
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-04-19 03:48:17 +0300

    pubspec update and debugbanner update

--- lib/app.dart
+++ lib/app.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Netflix Clone',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
