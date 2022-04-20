d907ca8424667d6833646da02b50f68439441f2e
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-04-20 23:52:40 +0300

    clean homescreen

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,17 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:netflix_clone/Widgets/Homescreen/homescreen_thumbnail.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          HomeScreenThumbnail(),
-        ],
-      ),
-    );
+    return Scaffold();
   }
 }
