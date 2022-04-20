d5737ce5f1d331a67ad27c8641b0ff29a3b4b223
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-04-20 23:52:14 +0300

    added new poster and buttons

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_clone/Widgets/Homescreen/homescreen_thumbnail.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          HomeScreenThumbnail(),
+        ],
+      ),
+    );
   }
 }
