9274b6db4ea0bbd7f78dec081792e1729cc46ad1
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-04-21 00:22:48 +0300

    added posterlist and listview

--- lib/Widgets/Homescreen/shows_list.dart
+++ lib/Widgets/Homescreen/shows_list.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_clone/Widgets/Homescreen/shows_card.dart';
 
 class ShowsList extends StatelessWidget {
   const ShowsList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var posterList = [
+      'lib/Utils/Img/spiderman_hc.png',
+      'lib/Utils/Img/spiderman_nwh.png',
+      'lib/Utils/Img/dont_look_up.png',
+      'lib/Utils/Img/the_amazing_spiderman.png',
+    ];
+    return ListView.builder(
+        itemBuilder: (context, index) =>
+            ShowsCard(img_asset: posterList[index]));
   }
 }
