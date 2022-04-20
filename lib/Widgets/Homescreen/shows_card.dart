76b3996f5b832acc169d07d48b106c18a3e43173
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-04-21 00:11:42 +0300

    created shows card

---
+++ lib/Widgets/Homescreen/shows_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ShowsCard extends StatelessWidget {
+  const ShowsCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
