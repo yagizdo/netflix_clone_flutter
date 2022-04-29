b04e0ef6e21b73345fe6fdf138ae31bb11e0b340
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-04-29 23:33:21 +0300

    updated text

--- lib/Widgets/Homescreen/KeepWatching/kw_card.dart
+++ lib/Widgets/Homescreen/KeepWatching/kw_card.dart
@@ -3,7 +3,12 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:netflix_clone/Utils/Colors/colors.dart';
 
 class KwCard extends StatelessWidget {
-  KwCard({Key? key, required this.img_url, required this.seasonNumber, required this.episodeNumber}) : super(key: key);
+  KwCard(
+      {Key? key,
+      required this.img_url,
+      required this.seasonNumber,
+      required this.episodeNumber})
+      : super(key: key);
   String img_url;
   int seasonNumber;
   int episodeNumber;
@@ -18,14 +23,34 @@ class KwCard extends StatelessWidget {
             Container(
               height: 156,
               width: 120,
-              margin: EdgeInsets.symmetric(horizontal: 8),
+              margin: const EdgeInsets.symmetric(horizontal: 8),
               decoration: BoxDecoration(
                 image: DecorationImage(
-                    image: AssetImage(img_url),
-                    fit: BoxFit.fill),
+                    image: AssetImage(img_url), fit: BoxFit.fill),
               ),
             ),
-            Positioned(child: SvgPicture.asset('lib/Utils/Icons/keep_icon.svg'))
+            Positioned(
+              top: 135,
+              child: Container(
+                alignment: Alignment.center,
+                width: MediaQuery.of(context).size.width / 1,
+                height: MediaQuery.of(context).size.height / 27,
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [black, Colors.transparent],
+                    begin: Alignment.center,
+                    end: Alignment.topCenter,
+                  ),
+                ),
+                child: Text(
+                  'S$seasonNumber:B$episodeNumber',
+                  style: const TextStyle(
+                      color: white, fontSize: 11, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Positioned(
+                child: SvgPicture.asset('lib/Utils/Icons/keep_icon.svg')),
           ],
         ),
         Container(
@@ -34,9 +59,15 @@ class KwCard extends StatelessWidget {
           width: MediaQuery.of(context).size.width / 3.4,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Icon(Icons.info,color: grey,),
-              Icon(Icons.more_vert_outlined,color: grey,),
+            children: const [
+              Icon(
+                Icons.info,
+                color: grey,
+              ),
+              Icon(
+                Icons.more_vert_outlined,
+                color: grey,
+              ),
             ],
           ),
         ),
