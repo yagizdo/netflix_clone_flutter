97535ae4a0d74cf4b6e9cd737e87bb5fd9a6f203
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-04-29 20:31:00 +0300

    The movie_list has been made dynamic.

--- lib/Widgets/Homescreen/films_list.dart
+++ lib/Widgets/Homescreen/films_list.dart
@@ -5,23 +5,18 @@ import 'package:netflix_clone/Widgets/Homescreen/film_card.dart';
 import '../../Utils/Colors/colors.dart';
 
 class ShowsList extends StatelessWidget {
-  const ShowsList({Key? key}) : super(key: key);
+  ShowsList({Key? key,required this.title,required this.posterList}) : super(key: key);
+  String title;
+  var posterList;
 
   @override
   Widget build(BuildContext context) {
-    var posterList = [
-      'lib/Utils/Img/iron_man_2_post.png',
-      'lib/Utils/Img/spiderman_hc_post.jpeg',
-      'lib/Utils/Img/the_prestige_post.jpg',
-      'lib/Utils/Img/spiderman_nwh_post.webp',
-      'lib/Utils/Img/wanted_film_post.jpg',
-    ];
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
           padding: const EdgeInsets.only(left: 8.0),
-          child: Text('Films',
+          child: Text(title,
               style: TextStyle(
                   color: white, fontWeight: FontWeight.bold, fontSize: 15.sp)),
         ),
