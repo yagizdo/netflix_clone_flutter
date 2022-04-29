c846e627179f3b9abaeeaf0beeb0db0a138ef89c
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-04-29 20:36:20 +0300

    updated homescreen

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -8,11 +8,22 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var filmPosterList = [
+      'lib/Utils/Img/iron_man_2_post.png',
+      'lib/Utils/Img/spiderman_hc_post.jpeg',
+      'lib/Utils/Img/the_prestige_post.jpg',
+      'lib/Utils/Img/spiderman_nwh_post.webp',
+      'lib/Utils/Img/wanted_film_post.jpg',
+    ];
     return Scaffold(
       extendBodyBehindAppBar: true,
       body: SingleChildScrollView(
         child: Column(
-          children: [HomescreenAB(), HomeScreenThumbnail(), Content()],
+          children: [
+            HomescreenAB(),
+            HomeScreenThumbnail(),
+            ContentList(title: 'Films', posterList: filmPosterList)
+          ],
         ),
       ),
     );
